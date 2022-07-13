04292885a97143888e660db84b4a5c52f1fe2144
guzengpei <1093569539@qq.com>
2022-07-13 11:52:15 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 头部是否固定
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 侧边栏logo是否显示
+   */
+  sidebarLogo: true
+}
